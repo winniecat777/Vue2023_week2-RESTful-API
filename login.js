f08c8c97e79e0da7bd85c1f1ef6beef7d3cc394e
winniecat777 <winniecat777@gmail.com>
2024-01-14 18:09:06 +0800

    edit

--- login.js
+++ login.js
@@ -1,1 +1,27 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
+createApp({
+  data() {
+    return {
+      user: {
+        username:"",
+        password:"",
+      },
+    }
+  },
+  methods: {
+    login() {
+      const apiUrl = 'https://vue3-course-api.hexschool.io/v2/admin/signin';
+      axios
+        .post(apiUrl, this.user)
+        .then((res) => {
+          const {token, expired} = res.data;
+          document.cookie = `hexToken=${token}; expires=${new Date(expired)}; path=/`;
+          window.location = 'products.html';
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        })
+    }
+  },
+}).mount('#app');
--- products.js
+++ products.js
@@ -1,1 +1,46 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
+createApp({
+  data() {
+    return {
+      apiUrl:'https://vue3-course-api.hexschool.io/v2',
+      apiPath:'winnie03',
+      products:[],
+      tempProduct:{}
+    };
+  },
+  methods: {
+    checkAdmin(){
+      const url = `${this.apiUrl}/api/user/check`;
+      axios
+        .post(url)
+        .then(() => {
+          console.log('驗證成功！');
+          this.getProducts();
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+          window.location = 'login.html';
+        })
+    },
+    getProducts() {
+      const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
+      axios
+        .get(url)
+        .then((res) => {
+          this.products = res.data.products;
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        })
+    },
+    openProduct(item) {
+      this.tempProduct = item;
+    }
+  },
+  mounted() {
+    const token = document.cookie.replase(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    axios.defaults.headers.common.Authorization = token;
+    this.checkAdmin()
+  }
+}).mount('#app')
